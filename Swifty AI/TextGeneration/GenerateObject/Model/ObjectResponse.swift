8df5920ea11161af1dcdc6c7f0e9a8489d8e2317
kartikayy007 <kartikayy6969@gmail.com>
2026-04-27 03:56:05 +0530

    refactor: remove Sendable constraints from ObjectResponse, GenerateObject, and MovieSuggestion models

--- Swifty AI/TextGeneration/GenerateObject/Model/ObjectResponse.swift
+++ Swifty AI/TextGeneration/GenerateObject/Model/ObjectResponse.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct ObjectResponse<T: Decodable & Sendable>: Sendable {
+public struct ObjectResponse<T: Decodable> {
     public let object: T
     public let usage: TokenUsage?
     public let finishReason: String?
